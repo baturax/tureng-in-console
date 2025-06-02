9ed7b88753b276f54d2d66771bedda27f6b8b0dd
Baturax <bai@baturax.com>
2025-06-02 03:52:51 +0300

    first commit

---
+++ bai.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func main() {
+
+	q := strings.Join(os.Args[1:], " ")
+	a := url.PathEscape(q)
+
+	req, err := http.NewRequest("GET", "https://tureng.com/en/turkish-english/"+a, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Set("User-Agent", "Mozilla/5.0")
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	doc.Find("tr.tureng-manual-stripe-even, tr.tureng-manual-stripe-odd").Each(func(i int, s *goquery.Selection) {
+		english := s.Find("td.en.tm a").Text()
+		turkish := s.Find("td.tr.ts a").Text()
+
+		if english != "" && turkish != "" {
+			fmt.Printf("%d: %s -> %s\n", i+1, english, turkish)
+		}
+	})
+}
